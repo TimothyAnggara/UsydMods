230c6df0872c70253facd4c0338b657be72ddd33
wwal7356 <wwal7356@uni.sydney.edu.au>
2023-03-25 23:49:33 +1100

    Support for simple web scrapes.

---
+++ backend/models/DueDateGetter.js
@@ -0,0 +1,36 @@
+const { parse } = require('node-html-parser');
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+ 
+function getData(url) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url);
+    
+    xhr.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var result = this.responseText;
+            valueCallBack(result);
+        }
+    };
+    xhr.send();
+}
+
+function valueCallBack(value){
+    const root = parse(value);
+    const table = root.getElementById("assessment-table");
+    const dates = [];
+
+    for (let i = 0; i < table.childNodes.length; i++) {
+        AssessmentName = root.querySelector("#assessment-table > tbody:nth-child("+i.toString()+") > tr.primary > td:nth-child(2) > b");
+        DueDate = root.querySelector("#assessment-table > tbody:nth-child("+i.toString()+") > tr.primary > td:nth-child(4)")
+        if (AssessmentName != null) {
+            var newDueDate = DueDate.text.toString().replace(/\s+/g, ' ').trim();
+            dates.push([AssessmentName.innerHTML,newDueDate]);
+        }
+    }
+
+    for (let i = 0; i < dates.length; i++) {
+        console.log(dates[i]);
+    }
+}
+
+getData("https://www.sydney.edu.au/units/COMP2123/2023-S1C-ND-CC");
